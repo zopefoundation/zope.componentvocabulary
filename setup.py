d321cd7baa705b796e535c1b7dd55da37b60b9a9
Stephan Richter <stephan.richter@gmail.com>
2013-02-25 12:13:21 -0500

    Preparing release 2.0.0a1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.componentvocabulary',
-      version='2.0.0a1.dev',
+      version='2.0.0a1',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Component vocabularies',
