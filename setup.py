98336ac3df23840186bf4f87b73790b0a6a3e4ca
Michael Howitz <mh@gocept.com>
2021-12-17 07:56:45 +0100

    Back to development: 2.3.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ TEST_REQUIRES = [
 ]
 
 setup(name='zope.componentvocabulary',
-      version='2.3.0',
+      version='2.3.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Component vocabularies',
